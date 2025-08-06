0e13f6116ef08ed4c55d29991e98dab8f248ab0e
kerber <roberto.oliveira-kerber@ext.ec.europa.eu>
2025-08-06 12:28:13 +0200

    Adicionando gunicorn ao requirements.txt

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, send_from_directory
 
 application = Flask(__name__)
 
-# Nova rota para servir o arquivo sw.js na raiz do site.
-# Isso é necessário para a verificação da Monetag.
+# Rota para servir o arquivo sw.js na raiz do site.
+# Isso permite que a Monetag encontre e verifique o arquivo.
 @application.route('/sw.js')
 def serve_sw():
     return send_from_directory('static', 'sw.js')
@@ -17,12 +17,10 @@ def home():
 @application.route('/convert', methods=['POST'])
 def convert():
     try:
-        # Pega o valor do formulário e converte para um número inteiro
         bytes_value = int(request.form['bytes'])
         unit = request.form['unit']
         result = None
 
-        # Realiza a conversão com base na unidade selecionada
         if unit == 'KB':
             result = f"{bytes_value / 1024:.2f} KB"
         elif unit == 'MB':
@@ -32,11 +30,9 @@ def convert():
         elif unit == 'TB':
             result = f"{bytes_value / (1024 ** 4):.2f} TB"
 
-        # Renderiza a página com o resultado da conversão
         return render_template('index.html', result=result)
     
     except (ValueError, KeyError):
-        # Em caso de erro na entrada de dados, exibe uma mensagem de erro
         error_message = "Entrada inválida. Por favor, insira um número válido."
         return render_template('index.html', error=error_message)
 
